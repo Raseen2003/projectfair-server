ba747b6981a9adf17442b8d2dddc717493beef34
Raseen2003 <mohammedrasenn000@gmail.com>
2025-01-12 15:07:42 +0530

    BOOK-ROOM

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -3,16 +3,71 @@ const projects = require('../models/projectModel')
 // add project
 
 exports.addProjectController = async (req,res)=>{
-    console.log("inside addProjectContoller");
+    console.log("inside addProjectController");
     const userId =  req.userId
 
 
-    console.log(userId);
-    console.log(req.body);
-    console.log(req.file);
-    
+    // console.log(userId);
+    // console.log(req.body);
+    // console.log(req.file);
+    const {title,languages,overview,github,website} = req.body
+    const projectImage = req.file.filename
+    try{
+        const existingProject = await projects.findOne({github})
+        if(existingProject){
+            res.status(406).json("Projetct Already exists... Please upload another!!!")
+        }else{
+            const newProject = new projects({
+                title,languages,overview,github,website,projectImage,userId
+            })
+            await newProject.save()
+            res.status(200).json(newProject)
+        }
+    }catch(err){
+        res.status(401).json(err)
+    }
     
 
-    res.status(200).json("Add project request recieved!!!")
+   
+    
+}
+
+// get home projects - guest user
+
+exports.getHomeProjectsContoller = async (req,res)=>{
+    console.log("inside getHomeProjectsContoller");
+    try{
+        const allHomeProjects = await projects.find().limit(3)
+        res.status(200).json(allHomeProjects)
+    }catch(err){
+        res.status(401).json(err)
+    }
+    
+}
+
+// get user projects - authorised user
+
+exports.getUserProjectsContoller = async (req,res)=>{
+    console.log("inside getUserProjectsContoller");
+    const userId = req.userId
+    try{
+        const allUserProjects = await projects.find({userId})
+        res.status(200).json(allUserProjects)
+    }catch(err){
+        res.status(401).json(err)
+    }
+    
+}
+
+// get all projects - auhrorised user
+
+exports.getAllProjectsContoller = async (req,res)=>{
+    console.log("inside getAllProjectsContoller");
+    try{
+        const allProjects = await projects.find()
+        res.status(200).json(allProjects)
+    }catch(err){
+        res.status(401).json(err)
+    }
     
 }
--- routes/router.js
+++ routes/router.js
@@ -12,5 +12,10 @@ router.post('/register',userController.registerContoller)
 router.post('/login',userController.loginContoller)
 // add-project - post 
 router.post('/add-project',jwtMiddleware,multerMiddleWare.single('projectImage'), projectController.addProjectController)
-
+// home-project - get 
+router.get('/home-projects',projectController.getHomeProjectsContoller)
+// user-project - get 
+router.get('/user-projects',jwtMiddleware,projectController.getUserProjectsContoller)
+// all-project - get 
+router.get('/all-projects',jwtMiddleware,projectController.getAllProjectsContoller)
 module.exports = router
